1d916e682d672a2a6b4fce187dd5577c30f76287
Anton3421 <bgx169@myy.haaga-helia.fi>
2023-09-12 21:21:44 +0300

    valmis tehtävä

--- App.js
+++ App.js
@@ -4,43 +4,62 @@ import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
 
 export default function App() {
 
-  const [text, setText] = useState('');
+  const [result, setResult] = useState(0);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  const addingButtonPressed = () => {
+    const sum = parseFloat(number1) + parseFloat(number2);
+    setResult(sum);
+  }
+  
+  const deductionButtonPressed = () => {
+    const deduct = parseFloat(number1) - parseFloat(number2);
+    setResult(deduct);
+  }
 
   return (
 
 
     <View style={styles.container}>
       <View>
-        <Text>Result: </Text>
+        <Text>Result: {result}</Text>
       </View>
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput keyboardType={"numeric"}
           style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => setText(text)}
+          onChangeText={number1 => setNumber1(number1)}
+          
         />
         <TextInput keyboardType={"numeric"}
           style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => setText(text)}
+          onChangeText={number2 => setNumber2(number2)}
+          
         />
       </View>
-      <View flexDirection='row'>
-        <Button onPress={buttonPressed} title="+"></Button>
-        <Button onPress={buttonPressed} title="-"></Button>
+      <View style={styles.buttonContainer}>
+        <Button onPress={addingButtonPressed} title="+"></Button>
+        <Button onPress={deductionButtonPressed} title="-"></Button>
         <StatusBar style="auto" />
       </View>
     </View>
   );
 }
 
-const buttonPressed = () => {
-
-}
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  inputContainer: {
+    flexDirection: 'column',
+    marginBottom: 20, 
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20, 
+    alignItems: 'flex-start',
   },
 });
